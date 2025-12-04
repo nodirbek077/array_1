ba8a1673e3cb5a2bfed13db5d311e283fa0fc0b1
Nodirbek <n.nurkulov@insuranceon.uz>
2025-12-04 10:05:54 +0500

    exercise-23 done and exercise-24 started

--- src/practice1/Exercise23.java
+++ src/practice1/Exercise23.java
@@ -1,7 +1,26 @@
 package practice1;
 
+import util.ArrayUtil;
+
+/**
+ * 23. n ta elementdan tashkil topgan massiv berilgan.
+ * Massiv elementlari orasida, chap qo'shnisidan katta bo'lgan elementlarining indeksini kamayish tartibida chiqaruvchi
+ * va ularning sonini chiqaruvchi programma tuzilsin.
+ */
 public class Exercise23 {
     public static void main(String[] args) {
+        int[] numbers = ArrayUtil.getArrayElements();
+        int n = numbers.length;
+        int counter = 0;
 
+        System.out.print("Chap qo'shnisidan katta bo'lgan elementlarning indeksi (kamayish tartibida): ");
+        for (int i = n - 1; i > 0; i--) {
+            if (numbers[i - 1] < numbers[i]) {
+                counter++;
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        System.out.print("Ularning soni: " + counter);
     }
 }
--- src/practice1/Exercise24.java
+++ src/practice1/Exercise24.java
@@ -1,7 +1,23 @@
 package practice1;
 
+import util.ArrayUtil;
+
+/**
+ * 24. n ta elementdan tashkil topgan massiv berilgan.
+ * Massiv elementlari orasidan monoton o'suvchi oraliqlar sonini chiqaruvchi programma tuzilsin.
+ * Namuna: [2,3,5,3,1,6]
+ * Monoton o'suvchi oraliq 2ta. (2,3,5) (1,6)
+ */
 public class Exercise24 {
     public static void main(String[] args) {
+        int[] numbers = ArrayUtil.getArrayElements();
+        int n = numbers.length;
 
+//        System.out.print(numbers[0] + " ");
+        for (int i = 1; i < n - 1; i++) {
+            if (numbers[i - 1] < numbers[i]) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
