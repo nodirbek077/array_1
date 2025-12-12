d4134bbd15b8dc2c473f4ff7e8d249cb08d8b23b
Nodirbek <n.nurkulov@insuranceon.uz>
2025-12-12 16:24:39 +0500

    exercise-14 remained and exercise-15 started

--- src/practice3/Exercise13.java
+++ src/practice3/Exercise13.java
@@ -1,5 +1,10 @@
 package practice3;
 
+import util.ArrayUtil;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * 13. n ta elementdan tashkil topgan massiv va k butun soni berilgan (1 < k < n).
  * Massiv elementlarini k ta o'rin o'ngga siljituvchi programma tuzilsin.
@@ -9,6 +14,62 @@ package practice3;
  */
 public class Exercise13 {
     public static void main(String[] args) {
+        int[] numbers = ArrayUtil.getArrayElements();
+        int n = numbers.length;
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number k: ");
+        int k = input.nextInt();
+
+        if (k >= n) {
+            System.out.println("K must be less than n");
+            return;
+        }
+
+        if (k <= 1 && k != 0) {
+            System.out.println("K must be greater than 1");
+            return;
+        }
+
+        if (k == 0) {
+            System.out.println(Arrays.toString(numbers));
+            return;
+        }
+
+        //1. create temporary array far saving last K ta element
+        int[] temp = new int[k];
+
+        //2. move to last k element ([0, 1, 2, 3, 4])
+        for (int i = 0; i < k; i++) {
+            temp[i] = numbers[n - k + i];
+        }
+
+        //3. remaining elements to move k ta
+        for (int i = n - 1; i >= k; i--) {
+            numbers[i] = numbers[i - k];
+        }
+
+        //4. need to move the elements that are in the temp array to the beginning of an array
+        for (int i = 0; i < k; i++) {
+            numbers[i] = temp[i];
+        }
+
+        //new changed array length
+        int newArrayLength = n - k;
+        int[] newArrayNumbers = new int[newArrayLength];
+        for (int i = 0; i < newArrayLength; i++) {
+            newArrayNumbers[i] = numbers[k + i];
+        }
+
+        //set to zero if counter reaches to k (k including)
+        int counter = 0;
+        for (int i = 0; i < newArrayLength; i++) {
+            counter++;
+
+            if (counter <= k) {
+                newArrayNumbers[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(newArrayNumbers));
     }
 }
--- src/practice3/Exercise15.java
+++ src/practice3/Exercise15.java
@@ -1,11 +1,40 @@
 package practice3;
 
+import util.ArrayUtil;
+
+import java.util.Arrays;
+
 /**
  * 15. n ta elementdan tashkil topgan massiv berilgan.
  * Massivning  elementidan i o'sish tartibi joylashtirilgan.
  */
 public class Exercise15 {
     public static void main(String[] args) {
+        int[] numbers = ArrayUtil.getArrayElements();
+        int n = numbers.length;
 
+        int min = numbers[0];
+        //[2, -1, 4, 9, -19, 40]
+        //[-1,                    4, 9, -19, 40]
+
+        //[-1, 2]
+        //1. first we need to find the smallest number in the array
+        for (int i = 1; i < n - 1; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        System.out.println("Minimum number is " + min);
+
+        numbers[0] = min;
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = 0; j < n -i; j++) {
+                if (numbers[j] > numbers[i]) {
+                    numbers[i] = numbers[j];
+                }
+            }
+        }
+        System.out.println(Arrays.toString(numbers));
     }
 }
