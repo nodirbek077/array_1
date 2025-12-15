2ccf85e140689913e940276603201a99ef945eab
Nodirbek <nodirbeknurqulov096@gmail.com>
2025-12-15 07:20:52 +0500

    just testing contributing day-to-day commit visible in github ui

--- src/practise4/Exercise4.java
+++ src/practise4/Exercise4.java
@@ -2,6 +2,8 @@ package practise4;
 
 import util.ArrayUtil;
 
+import java.util.Arrays;
+
 /**
  * 4. n ta elementdan iborat butun sonlardan tashkil topgan a massiv berilgan.
  * a massivning juft elementlaridan tashkil topgan b massivini hosil qiling.
@@ -11,5 +13,8 @@ public class Exercise4 {
     public static void main(String[] args) {
         int[] a = ArrayUtil.getArrayElements();
         int[] b = new int[a.length];
+
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
     }
 }
