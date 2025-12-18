edf22fa924758d0b815f8ad1db143e3d4a16f352
Nodirbek <nodirbeknurqulov096@gmail.com>
2025-12-18 21:41:25 +0500

    exercise14 finished

--- src/practise5/Exercise14.java
+++ src/practise5/Exercise14.java
@@ -1,11 +1,39 @@
 package practise5;
 
+import util.ArrayUtil;
+
+import java.util.Arrays;
+
 /**
  * 13. n ta elementdan tashkil topgan massiv berilgan.
  * Massivga, massivning toq elementlarini 2 marta qo'shuvchi programma tuzilsin.
  */
 public class Exercise14 {
     public static void main(String[] args) {
+        int[] nums = ArrayUtil.getArrayElements();
 
+        //1. count odd elements in array
+        int countOddElement = 0;
+        for (int x : nums) {
+            if (x % 2 == 1) {
+                countOddElement++;
+            }
+        }
+
+        //2. create new array
+        int[] result = new int[nums.length + countOddElement];
+
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            result[j++] = nums[i];
+
+            if (nums[i] % 2 != 0) {
+                result[j++] = nums[i];
+            }
+        }
+
+        System.out.println("Old array: " + Arrays.toString(nums));
+        System.out.println("New array: " + Arrays.toString(result));
     }
 }
