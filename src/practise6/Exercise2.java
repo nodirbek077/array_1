51508a47e47808932f5e32d0df7f1f36f2bcd237
Nodirbek <nodirbeknurqulov096@gmail.com>
2025-12-21 22:12:31 +0500

    exercise2 continued

--- src/practise6/Exercise2.java
+++ src/practise6/Exercise2.java
@@ -2,6 +2,8 @@ package practise6;
 
 import util.ArrayUtil;
 
+import java.util.Arrays;
+
 /**
  * 2. n ta elementdan tashkil topgan massiv berilgan. Oddiy tanlash (selection sort) algoritmi orqali massivni o'sish tartibida
  * chiqaruvchi programma tuzilsin.
@@ -11,18 +13,28 @@ public class Exercise2 {
     public static void main(String[] args) {
         int[] nums = ArrayUtil.getArrayElements();
 
-        //[1, -2, 8, 16, -4, 11, 5]
-        int minIndex = 0;
+        //[3, 1, 8, 4, 2, 7]
         for (int i = 0; i < nums.length; i++) {
+//            System.out.println("i = " + i);
+            int min = nums[i];
+
             for (int j = i + 1; j < nums.length; j++) {
-
-                if (nums[i] > nums[j]) {
-
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
+                if (nums[j] < min) {
+                    min = nums[j];
+                    System.out.println(i + ", min:" + min);
                 }
             }
+
+//            System.out.println(minIndex);
+/*
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[minIndex]) {
+                    int temp = nums[i];
+                    nums[i] = nums[minIndex];
+                    nums[minIndex] = temp;
+                }
+            }*/
         }
+//        System.out.println(Arrays.toString(nums));
     }
 }
