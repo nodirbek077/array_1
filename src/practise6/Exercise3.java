da312b26b286538b1a70aa7f40e78ca3497f2643
Nodirbek <n.nurkulov@insuranceon.uz>
2025-12-22 12:00:34 +0500

    some changes added to Exercise3

--- src/practise6/Exercise3.java
+++ src/practise6/Exercise3.java
@@ -2,6 +2,8 @@ package practise6;
 
 import util.ArrayUtil;
 
+import java.util.Arrays;
+
 /**
  * 3. n ta elementdan tashkil topgan massiv berilgan.
  * Oddiy qo'shish (insertion sort) algoritmi orqali massivni o'sish tartibida chiqaruvchi programma tuzilsin.
@@ -13,5 +15,21 @@ import util.ArrayUtil;
 public class Exercise3 {
     public static void main(String[] args) {
         int[] nums = ArrayUtil.getArrayElements();
+        //[3, 1, 8, 4, 2, 7]
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i - 1; j > 0; j--) {
+                //1. chack element if less than previous element
+                if (nums[i] < nums[i - 1]) {
+                    //2. move item to right
+//                    int temp = nums[i];
+//                    nums[i] = nums[j];
+//                    nums[j] = temp;
+
+                    nums[j + 1] = nums[j];
+//                    nums[i - 1] = nums[i];
+                }
+            }
+        }
+        System.out.println(Arrays.toString(nums));
     }
 }
